50a721f5034872a7d708426d5e4add66de14db6e
Swati Modi <swatimodi1609@gmail.com>
2021-02-08 18:06:33 +0530

    sliding window

--- leetcode/378_kth_smallest_sorted_matx.py
+++ leetcode/378_kth_smallest_sorted_matx.py
@@ -18,3 +18,39 @@ class Solution(object):
             i+=1
         
         return heap[0]
+
+
+#         if len(matrix) == 0:
+#             return 0
+        
+#         heap = []
+#         for row in matrix:
+#             heap = list(heapq.merge(heap, row))
+        
+#         return heap[k-1]
+
+
+# 120 ms binary search
+# class Solution(object):
+#     def kthSmallest(self, matrix, k):
+#         """
+#         :type matrix: List[List[int]]
+#         :type k: int
+#         :rtype: int
+#         """
+#         import sys
+#         l, r = matrix[0][0], matrix[-1][-1]
+#         while l < r:
+#             m = (l + r)//2
+#             j = len(matrix[0]) - 1 
+#             cnt = 0 
+#             for i in range(len(matrix)):
+#                 while j >= 0 and matrix[i][j] > m:
+#                     j -= 1 
+#                 cnt += j + 1 
+#             if cnt >= k:
+#                 r = m
+#             else:
+#                 l = m + 1 
+#         return r 
+
